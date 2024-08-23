5ca9e469eec79bcebfebe104788ce812b2b90ed7
Palady56 <paladyultra@gmail.com>
2024-08-23 21:51:46 +0300

    init

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
       <Header/>
       <Routes>
         <Route index element={<ProductList/>}/>
-        <Route path={'form'} element={<Form/>}/>
+        <Route path={'/form'} element={<Form/>}/>
       </Routes>
     </div>
   )
